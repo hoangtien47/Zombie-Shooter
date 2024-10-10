9253df1b3390fe0557f238bc7f9eec7841266af2
hoangtien47 <hoangtien472003@gmail.com>
2024-10-11 02:18:01 +0700

    first commit/setup game project

---
+++ Source/ZombieShooter/ZombieShooter.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZombieShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZombieShooter, "ZombieShooter" );
+
+DEFINE_LOG_CATEGORY(LogZombieShooter)
+ 
---
+++ Source/ZombieShooter/ZombieShooterGameMode.cpp
@@ -0,0 +1,30 @@
+
+
+
+
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZombieShooterGameMode.h"
+#include "ZombieShooterPlayerController.h"
+#include "ZombieShooterCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AZombieShooterGameMode::AZombieShooterGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = AZombieShooterPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	// set default controller to our Blueprinted controller
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	if (PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
